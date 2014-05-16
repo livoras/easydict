3b1a58bc4ce0cb94229822d352b2fe1d4e85d309
livoras <me@livoras.com>
2014-05-16 15:27:13 +0800

    distribution support

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- codeing: utf-8 -*-
+from distutils.core import setup
+import easydict
+
+setup(
+  name="easydict",
+  version=easydict.__version__,
+  url="https://github.com/livoras/easydict",
+  license="GPL v2",
+  author="Livoras",
+  author_email="me@livoras.com",
+  description="Super easy and lightweight dictionary tool for linux.",
+  scripts=["scripts/easydict"],
+  packages=["easydict"],
+  include_package_data=True
+)
---
+++ tests/test_translate.py
@@ -0,0 +1,2 @@
+def test_get_translate_data():
+  assert 'i' in 'fuck'
